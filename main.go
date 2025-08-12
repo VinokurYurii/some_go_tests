93176913f46cf9f6435138bbca021d4bbe2f6500
Vynokur Yurii <vynokur.yurii@gmail.com>
2025-08-12 20:35:14 +0300

    String to hex

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Enter hex number or 'stop' to exit:")
+
+	var input string
+
+	for {
+		fmt.Scanln(&input)
+
+		if strings.ToLower(input) == "stop" {
+			break
+		}
+
+		i := new(big.Int)
+
+		if _, ok := i.SetString(processHex(input), 16); !ok {
+			fmt.Println("Invalid hex number")
+			continue
+		}
+
+		fmt.Println(i)
+	}
+}
+
+func processHex(hexStr string) string {
+	return strings.TrimPrefix(hexStr, "0x")
+}
